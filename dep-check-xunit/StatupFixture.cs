756fdcdb7c18023f0332ddf4b5a65aaf418384d5
tomap <piartt@gmail.com>
2018-12-19 10:03:12 +0100

    demo App.Mettrics issue

---
+++ dep-check-xunit/StatupFixture.cs
@@ -0,0 +1,51 @@
+﻿using DepCheck;
+using DepCheck.Interfaces;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace dep_check_xunit
+{
+    public class StartupFixture : IClassFixture<WebApplicationFactory<Startup>>
+    {        
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public StartupFixture(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/env", "1")]
+        [InlineData("/env", "2")]
+        [InlineData("/env", "3")]
+        public async Task CheckMonitoringEndpointsContentTypesAsync(string url, string contentType)
+        {
+            // Arrange
+            var mockMyService = new Mock<IMyService>();
+            mockMyService.Setup(ms => ms.Run());
+
+            var client = _factory.WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(services =>
+                    {
+                        services.AddScoped(_ => mockMyService.Object);
+                    });
+                })
+                .CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal(contentType,
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
---
+++ dep-check/AppMetrics/WebHostBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using App.Metrics;
+using App.Metrics.AspNetCore;
+using App.Metrics.AspNetCore.Health;
+using App.Metrics.Formatters;
+using App.Metrics.Formatters.Prometheus;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepCheck.AppMetrik
+{
+    public static class WebHostBuilderExtensions
+    {
+        public static IWebHostBuilder UseAppMetrics(this IWebHostBuilder hostBuilder)
+        {
+            var metrics = AppMetrics.CreateDefaultBuilder()
+                                .OutputMetrics.AsPrometheusPlainText()
+                                .Build();
+
+            return hostBuilder
+                .UseHealth()
+                .ConfigureMetrics(metrics)
+                .UseMetrics(o =>
+                {
+                    o.EndpointOptions = 
+                        endpointsOptions => endpointsOptions
+                                                .MetricsTextEndpointOutputFormatter = metrics
+                                                                                        .OutputMetricsFormatters
+                                                                                        .GetType<MetricsPrometheusTextOutputFormatter>();
+                });
+        }
+    }
+}
